f9079e549eb74142f450df65d5b60cb651b8b045
Patty <patrykbarnas@yahoo.com>
2024-02-15 21:22:02 -0600

    refactored router, backend rest api

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.listen(5000, () => console.log("server listening on port"));
+const productRoutes = require("./routes/products");
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.use("/products", productRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message || "Something went wrong.";
+  res.status(status).json({ message: message });
+});
+
+app.listen(8080);
---
+++ backend/data/products.js
@@ -0,0 +1,14 @@
+const fs = require("node:fs/promises");
+
+async function readData() {
+  const data = await fs.readFile("products.json", "utf8");
+  return JSON.parse(data);
+}
+
+async function getAll() {
+  const storedData = await readData();
+  if (!storedData.products) {
+    throw new Error("Products were not found");
+  }
+  return storedData.products;
+}
